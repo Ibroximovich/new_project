6acc0ef202c0b21c6da431e20d5d7a1964fba36c
Ibroximovich <azamovsarvar555@gmail.com>
2025-07-05 07:19:43 +0500

    update

---
+++ Js/main.js
@@ -0,0 +1,6 @@
+function HandleBtn() {
+  let openDiv = document.querySelector(".open-div")
+  let herSection = document.querySelector(".hero-section")
+   herSection.classList.toggle("hidden")
+    openDiv.classList.toggle("hidden")
+}
